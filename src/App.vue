8c6a2aacb2221c598c100ec4e807fbe2709dcd27
EngAliAbbas <116305973+EngAliAbbas@users.noreply.github.com>
2023-08-17 05:55:51 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <egypt-map/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EgyptMap from './components/EgyptMap.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EgyptMap
   }
 }
 </script>
